554e3bce2c307d1258ca01dfd309b6e8f356daf3
THE PUNISHER <artyom.pennini@gmail.com>
2026-08-15 15:17:08 +0200

    Update euro-depositare.js

--- plugins/euro-depositare.js
+++ plugins/euro-depositare.js
@@ -248,7 +248,7 @@ const renderDepositHTML = (props) => {
             <div class="footer-timestamp">${props.timestamp}</div>
         </div>
         
-        <div class="watermark">✧˚🩸 varebot 🕊️˚✧</div>
+        <div class="watermark">✧˚🩸 𝟴𝟴𝟴-𝗕𝗢𝗧 🕊️˚✧</div>
     </div>
 </body>
 </html>`;
@@ -398,7 +398,7 @@ let handler = async (m, { args, conn }) => {
         await conn.sendMessage(m.chat, { 
             image: mediaBuffer, 
             caption: successMessage, 
-            footer: '✧˚🩸 varebot 🕊️˚✧', 
+            footer: '✧˚🩸 𝟴𝟴𝟴-𝗕𝗢𝗧 🕊️˚✧', 
             buttons: buttons, 
             headerType: 4 
         }, { quoted: m });
@@ -408,7 +408,7 @@ let handler = async (m, { args, conn }) => {
         console.error("Errore nella creazione dell'immagine:", error);
         await conn.sendMessage(m.chat, { 
             text: successMessage, 
-            footer: '✧˚🩸 varebot 🕊️˚✧', 
+            footer: '✧˚🩸 𝟴𝟴𝟴-𝗕𝗢𝗧 🕊️˚✧', 
             buttons: buttons, 
             headerType: 1 
         }, { quoted: m });
